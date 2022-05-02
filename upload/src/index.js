b9a4ff18c9c373f1daf81343c839b84f2688b966
Renan Lima <86834473+RenanLima7@users.noreply.github.com>
2022-05-02 16:56:54 -0300

    Add files via upload

---
+++ upload/src/index.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const flash = require("connect-flash");
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
+const app = express();
+
+const routes = require("./routes");
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(cookieParser("keyboard cat"));
+app.use(
+  session({
+    secret: "SecretStringForSession",
+    cookie: { maxAge: 60000 },
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+app.use(flash());
+
+app.use(routes);
+
+app.set("view engine", "ejs");
+app.set("views", "./src/views");
+app.use(express.static(__dirname + "../../public"));
+
+app.get("/", (req, res) => {
+  res.render("home", { title: "Loja de Produtos" });
+});
+
+app.get("/home", (req, res) => {
+  res.render("home", { title: "Loja de Produtos" });
+});
+
+app.listen(3000);
